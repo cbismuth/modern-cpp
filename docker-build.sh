ca9cc9ca87ffda6742906f7157c8c5d023504947
Christophe Bismuth <christophe.bismuth@gmail.com>
2023-06-28 11:57:33 +0200

    Add code coverage support

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,23 @@
 #/usr/bin/env bash
 
-docker build --progress=plain -t modern-cpp/latest .
+set -e
+
+export BUILD_DIR="cmake-build-debug"
+
+# Clear scroll history
+printf "\33c\e[3J"
+
+# Override language support
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# Delete build output directory
+rm -Rf ${BUILD_DIR}
+
+# Build Docker image
+docker build --progress=plain -t modern-cpp:latest .
+
+# Copy build output directory from Docker container to host
+CONTAINER_ID=$(docker create modern-cpp:latest)
+docker cp ${CONTAINER_ID}:/opt/modern-cpp/${BUILD_DIR} ${BUILD_DIR}
+docker rm -v ${CONTAINER_ID}
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,30 +2,54 @@
 
 set -e
 
-NC='\033[0m'
-CYAN='\033[0;36m'
+export BUILD_DIR="cmake-build-debug"
 
-echo "${CYAN}Format source code ...${NC}"
-clang-format -i src/*.[ch]pp
+export NC="\033[0m"
+export CYAN="\033[0;36m"
+export SEPARATOR="========================================================================"
 
-echo "${CYAN}Run CMake to generate Makefile project ...${NC}"
-mkdir -p build
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build --config Debug -- -j $(nproc)
+log() {
+      echo "${CYAN}${1}${NC}"
+}
 
-echo "${CYAN}Build project ...${NC}"
-(cd build && make)
+section() {
+      log "${SEPARATOR}"
+      log "${1}"
+      log "${SEPARATOR}"
+}
 
-echo "${CYAN}Run tests with memcheck support ...${NC}"
-ctest --test-dir    build    \
-      --parallel    $(nproc) \
-      --test-action memcheck \
-      --schedule-random      \
-      --output-on-failure    \
+section "Format source code"
+clang-format --verbose --Werror -i src/*.[ch]pp
+
+section "Generate CMake project"
+cmake -S . -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON
+
+section "Build CMake project"
+cmake --build ${BUILD_DIR} --config Debug -- -j $(nproc)
+
+section "Build Makefile project"
+(cd ${BUILD_DIR} && make)
+
+section "Run tests with memcheck support"
+ctest --test-dir    ${BUILD_DIR} \
+      --parallel    $(nproc)     \
+      --test-action memcheck     \
+      --schedule-random          \
+      --output-on-failure        \
       --force-new-ctest-process
 
-echo "${CYAN}Print memcheck reports ...${NC}"
-find build/Testing/Temporary -maxdepth 1 -name "MemoryChecker*.log" -print -exec cat "{}" \;
+section "Print memcheck reports with filenames"
+find ${BUILD_DIR}/Testing/Temporary -maxdepth 1 -name "MemoryChecker*.log" -print -exec cat "{}" \;
 
-echo "${CYAN}Run static analysis ...${NC}"
+section "Generate code coverage reports"
+cd ${BUILD_DIR}
+lcov --directory . --capture --output-file coverage.info
+lcov --directory . --remove coverage.info '/usr/*' --output-file coverage.info.cleaned
+genhtml -o coverage coverage.info.cleaned
+cd -
+
+section "Run static analysis"
 clang-tidy src/*.[ch]pp -- -std=c++20 -I/usr/local/include
+
+log "Build output is located in the [${BUILD_DIR}] directory"
+log "Code coverage reports are rooted at [${BUILD_DIR}/coverage/index.html]"
